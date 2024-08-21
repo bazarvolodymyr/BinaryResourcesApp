5b82a996edd36075c7adb33d0cadbd5f4c3a7f49
bazarvolodymyr <bazar.volodymyr@gmail.com>
2024-08-21 12:23:29 +0300

    final

--- BinaryResourcesApp/MainWindow.xaml.cs
+++ BinaryResourcesApp/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ namespace BinaryResourcesApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<BitmapImage> _images = new List<BitmapImage>();
+        private int _currentImage = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -23,17 +25,36 @@ namespace BinaryResourcesApp
 
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                string path = Environment.CurrentDirectory;
+                _images.Add(new BitmapImage(new Uri(@"\Images\6t-YKf4lQXk.jpg", UriKind.Relative)));
+                _images.Add(new BitmapImage(new Uri(@"\Images\aOxNgzTOevc.jpg", UriKind.Relative)));
+                _images.Add(new BitmapImage(new Uri(@"\Images\NT3FLA4u35o.jpg", UriKind.Relative)));
+                imageHolder.Source = _images[_currentImage];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnPreviousImage_Click(object sender, RoutedEventArgs e)
         {
-
+            if (--_currentImage < 0)
+            {
+                _currentImage = _images.Count - 1;
+            }
+            imageHolder.Source = _images[_currentImage];
         }
 
         private void btnNextImage_Click(object sender, RoutedEventArgs e)
         {
-
+            if(++_currentImage >= _images.Count)
+            {
+                _currentImage = 0;
+            }
+            imageHolder.Source= _images[_currentImage];
         }
     }
 }
